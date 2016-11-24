ecbcefc16acc7099db10adf0f04339616a91e492
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2016-11-24 17:48:10 +0700

    add test

--- test/nine-gag/nine_gag_test.rb
+++ test/nine-gag/nine_gag_test.rb
@@ -14,6 +14,25 @@ describe NineGag do
       posts = NineGag.index('hot', @posts.last[:id])
       assert_equal posts.class, Array
     end
+
+    it 'should not nil if gif post' do
+      post = NineGag.index('gif/hot').first
+      assert post[:media] != nil
+    end
+
+    it 'should return Fixnum of points' do
+      assert_equal @posts.first[:points].class, Fixnum
+    end
+
+    it 'should return Fixnum of comments count' do
+      assert_equal @posts.first[:comments_count].class, Fixnum
+    end
+
+    it 'should return true if nsfw post' do
+      post = NineGag.index('nsfw/hot').first
+
+      assert post[:nsfw]
+    end
   end
 
   describe '#show' do
