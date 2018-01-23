ea6f981eeaeed20ce7ffa548a2b33d9659e34e07
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-23 12:13:12 +0700

    change result hash

--- lib/nine-gag.rb
+++ lib/nine-gag.rb
@@ -59,9 +59,27 @@ class NineGag
 
     begin
       result = RestClient.get(url, headers)
+      data =
+        JSON.parse(result.body, symbolize_names: true)[:data][:posts].map do |post|
+          {
+            id: post[:id],
+            title: post[:title],
+            url: post[:url],
+            comments_count: post[:commentsCount],
+            points: post[:upVoteCount],
+            media: {
+              image: post[:images][:image700][:url],
+              poster: post[:images][:image460][:url],
+              mp4: post[:images][:image460sv][:url],
+              webvm: post[:images][:image460svwm][:url]
+            },
+            tags: post[:tags].map {|t| t[:key]}
+          }
+        end
+
       {
         status:  "success",
-        data: JSON.parse(result.body, symbolize_names: true)[:data][:posts]
+        data: data
       }
     rescue RestClient::ExceptionWithResponse => e
       {
